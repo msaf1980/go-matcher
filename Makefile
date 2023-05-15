583b31290cb8052516e924395d74bd685ea25e58
msaf1980 <msaf1980@gmail.com>
2023-05-15 09:59:06 +0500

    expand: break expand on star

--- Makefile
+++ Makefile
@@ -25,15 +25,22 @@ test:
 
 # deep coverage (require Golang 1.20 or later)
 coverage: FORCE
+	# glob
 	${GO} test -coverprofile cover_glob.out -coverpkg=./glob,./pkg/items,./pkg/escape,./pkg/utils ./glob
 	${GO} tool cover -html=cover_glob.out -o cover_glob.html
+	# gglob
 	${GO} test -coverprofile cover_gglob.out -coverpkg=./gglob,./glob,./pkg/items,./pkg/escape,./pkg/utils ./gglob
 	${GO} tool cover -html=cover_gglob.out -o cover_gglob.html
-	# ${GO} test -coverprofile cover_gtags.out -coverpkg=./gtags,./pkg/items,./pkg/escape ./gtags
-	# ${GO} tool cover -html=cover_gtags.out -o cover_gtags.html
+	# gtags
+	${GO} test -coverprofile cover_gtags.out -coverpkg=./gtags,./pkg/items,./pkg/escape ./gtags
+	${GO} tool cover -html=cover_gtags.out -o cover_gtags.html
+	# expand
+	${GO} test -coverprofile cover_expand.out -coverpkg=./expand,./pkg/items,./pkg/escape ./expand
+	${GO} tool cover -html=cover_expand.out -o cover_expand.html
+	# ALL
 	# gocovmerge cover_items.out cover_escape.out cover_utils.out cover_gglob.out cover_gtags.out > cover.out
-	#${GO} test -coverprofile cover.out -coverpkg=./glob,./gglob,./gtags,./pkg/items,./pkg/escape,./pkg/utils ./...
-	${GO} test -coverprofile cover.out -coverpkg=./gglob,./glob,./pkg/items,./pkg/escape,./pkg/utils ./...
+	#${GO} test -coverprofile cover.out -coverpkg=./glob,./gglob,./gtags,./expand,./pkg/items,./pkg/escape,./pkg/utils ./...
+	${GO} test -coverprofile cover.out -coverpkg=./gglob,./glob,./expand,./pkg/items,./pkg/escape,./pkg/utils ./...
 	${GO} tool cover -html=cover.out -o cover.html
 
 lint:
--- expand/expand_test.go
+++ expand/expand_test.go
@@ -8,11 +8,145 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func Test_parseExpr(t *testing.T) {
+	tests := []struct {
+		in   string
+		want []expression
+	}{
+		{
+			in: "x{12,b}xxxxx",
+			want: []expression{
+				{body: "x"},
+				{typ: expList, body: "{12,b}", list: []string{"12", "b"}},
+				{body: "xxxxx"},
+			},
+		},
+		{
+			in: "ab[124-5]xxxxx",
+			want: []expression{
+				{body: "ab"},
+				{typ: expRunes, body: "[124-5]", runes: runesRangeExpandMust("124-5")},
+				{body: "xxxxx"},
+			},
+		},
+		// complex, not expand at now
+		{
+			in: "{x{12,{}}xxxxx",
+			want: []expression{
+				{typ: expWildcard, body: "{x{12,{}}"},
+				{body: "xxxxx"},
+			},
+		},
+		{
+			in: "x{12,{}}{{,13}",
+			want: []expression{
+				{body: "x"},
+				{typ: expWildcard, body: "{12,{}}{{,13}"},
+			},
+		},
+		{
+			in: "{x{12,{}}{{,13}",
+			want: []expression{
+				{typ: expWildcard, body: "{x{12,{}}{{,13}"},
+			},
+		},
+		{
+			in: "{{x{12,{}}{{,13}",
+			want: []expression{
+				{typ: expWildcard, body: "{{x{12,{}}{{,13}"},
+			},
+		},
+		// unclosed
+		{
+			in: "}some{",
+			want: []expression{
+				{typ: expWildcard, body: "}some{"},
+			},
+		},
+		{
+			in: "}some",
+			want: []expression{
+				{typ: expWildcard, body: "}"},
+				{body: "some"},
+			},
+		},
+		{
+			in: "some{",
+			want: []expression{
+				{body: "some"},
+				{typ: expWildcard, body: "{"},
+			},
+		},
+		{
+			in: "]some[",
+			want: []expression{
+				{typ: expWildcard, body: "]some["},
+			},
+		},
+		{
+			in: "]some",
+			want: []expression{
+				{typ: expWildcard, body: "]"},
+				{body: "some"},
+			},
+		},
+		{
+			in: "some[",
+			want: []expression{
+				{body: "some"},
+				{typ: expWildcard, body: "["},
+			},
+		},
+		// star
+		{
+			in: "x[124-5]a*xxxx",
+			want: []expression{
+				{body: "x"},
+				{typ: expRunes, body: "[124-5]", runes: runesRangeExpandMust("124-5")},
+				{body: "a"},
+				{typ: expWildcard, body: "*"},
+				{body: "xxxx"},
+			},
+		},
+		{
+			in: "x*[124-5]xxxxx",
+			want: []expression{
+				{body: "x"},
+				{typ: expWildcard, body: "*[124-5]"},
+				{body: "xxxxx"},
+			},
+		},
+		{
+			in: "x?[124-5]xxxxx",
+			want: []expression{
+				{body: "x"},
+				{typ: expWildcard, body: "?[124-5]"},
+				{body: "xxxxx"},
+			},
+		},
+		{
+			in: "x[*124-5]xxxxx",
+			want: []expression{
+				{body: "x"},
+				{typ: expWildcard, body: "[*124-5]"},
+				{body: "xxxxx"},
+			},
+		},
+	}
+	for n, tt := range tests {
+		t.Run(fmt.Sprintf("[%d] %s", n, tt.in), func(t *testing.T) {
+			gotExps := parseExpr(tt.in)
+			assert.Equal(t, tt.want, gotExps, "exps")
+		})
+	}
+}
+
 func TestExpand(t *testing.T) {
 	type data struct {
-		in  string
-		max int
-		out []string
+		in    string
+		max   int
+		depth int
+		out   []string
 	}
 
 	tests := []data{
@@ -22,6 +156,7 @@ func TestExpand(t *testing.T) {
 		{in: "a{b,,c}d", max: -1, out: []string{"abd", "ad", "acd"}},
 		{in: "a{b,c,}d", max: -1, out: []string{"abd", "acd", "ad"}},
 		{in: "a{b,}d", max: -1, out: []string{"abd", "ad"}},
+		{in: "a{b}d", max: -1, out: []string{"abd"}},
 		{in: "a{b,c,}d", max: -1, out: []string{"abd", "acd", "ad"}},
 		{in: "[2-4]", max: -1, out: []string{"2", "3", "4"}},
 		{in: "[24]", max: -1, out: []string{"2", "4"}},
@@ -32,55 +167,30 @@ func TestExpand(t *testing.T) {
 		{in: "as{12,32}[a-c]{2}", max: 2, out: []string{"as12[a-c]2", "as32[a-c]2"}},
 		{in: "as{12,32}[a-c]{2}", max: 5, out: []string{"as12[a-c]2", "as32[a-c]2"}},
 		{in: "as{12,32}[a-c]{2}", max: 6, out: []string{"as12a2", "as12b2", "as12c2", "as32a2", "as32b2", "as32c2"}},
-		{in: "as{12,32}[a-c]{2}", max: -2, out: []string{"as12[a-c]2", "as32[a-c]2"}},                                 // expand only first founded node
-		{in: "as{12,32}{2}[a-c]", max: -2, out: []string{"as122[a-c]", "as322[a-c]"}},                                 // expand only first founded node
-		{in: "as{12,32}2[a-c]", max: -2, out: []string{"as122[a-c]", "as322[a-c]"}},                                   // expand only first founded node
-		{in: "as{12,32}[a-c]{2}", max: -3, out: []string{"as12a2", "as12b2", "as12c2", "as32a2", "as32b2", "as32c2"}}, // expand only two founded nodes
-		{in: "as{12,32}2[a-c]", max: -3, out: []string{"as122a", "as122b", "as122c", "as322a", "as322b", "as322c"}},   // expand only two founded nodes
-		{in: "as{12,32}{2}[a-c]", max: -3, out: []string{"as122a", "as122b", "as122c", "as322a", "as322b", "as322c"}}, // expand only two founded nodes
-		{in: "as{12,32}[a-c]{2,a}", max: -3, out: []string{ // expand only two founded nodes
+		{in: "as{12,32}[a-c]{2}", max: -1, depth: 1, out: []string{"as12[a-c]2", "as32[a-c]2"}},                                 // expand only first founded node
+		{in: "as{12,32}{2}[a-c]", max: -1, depth: 1, out: []string{"as122[a-c]", "as322[a-c]"}},                                 // expand only first founded node
+		{in: "as{12,32}2[a-c]", max: -1, depth: 1, out: []string{"as122[a-c]", "as322[a-c]"}},                                   // expand only first founded node
+		{in: "as{12,32}[a-c]{2}", max: -1, depth: 2, out: []string{"as12a2", "as12b2", "as12c2", "as32a2", "as32b2", "as32c2"}}, // expand only two founded nodes
+		{in: "as{12,32}2[a-c]", max: -1, depth: 2, out: []string{"as122a", "as122b", "as122c", "as322a", "as322b", "as322c"}},   // expand only two founded nodes
+		{in: "as{12,32}{2}[a-c]", max: -1, depth: 2, out: []string{"as122a", "as122b", "as122c", "as322a", "as322b", "as322c"}}, // expand only two founded nodes
+		{in: "as{12,32}[a-c]{2,a}", max: -1, depth: 2, out: []string{ // expand only two founded nodes
 			"as12a{2,a}", "as12b{2,a}", "as12c{2,a}", "as32a{2,a}", "as32b{2,a}", "as32c{2,a}",
 		}},
-		{in: "as{12,32}[a-c]{2,a}", max: -4, out: []string{ // expand only three founded nodes
+		{in: "as{12,32}[a-c]{2,a}", max: -1, depth: 3, out: []string{ // expand only three founded nodes
 			"as12a2", "as12aa", "as12b2", "as12ba", "as12c2", "as12ca", "as32a2", "as32aa", "as32b2", "as32ba", "as32c2", "as32ca",
 		}},
+		// star
+		{in: "a{b,c}*d", max: -1, out: []string{"ab*d", "ac*d"}},
+		{in: "a*{b,c}d", max: -1, out: []string{"a*{b,c}d"}}, // no reverse expand
+		{in: "a?{b,c}d", max: -1, out: []string{"a?{b,c}d"}}, // no reverse expand
+		{in: "a{*b,c}d", max: -1, out: []string{"a{*b,c}d"}},
 	}
 
 	for n, tt := range tests {
-		t.Run(fmt.Sprintf("[%d] [%d] %s", n, tt.max, tt.in), func(t *testing.T) {
-			result, err := Expand(tt.in, tt.max)
+		t.Run(fmt.Sprintf("[%d] [%d:%d] %s", n, tt.max, tt.depth, tt.in), func(t *testing.T) {
+			result, err := Expand(tt.in, tt.max, tt.depth)
 			require.NoError(t, err, "input %q", tt.in)
 			assert.Equal(t, tt.out, result, "input %q", tt.in)
 		})
-	}
-}
-
-func TestGetPair(t *testing.T) {
-	type data struct {
-		in    string
-		start int
-		stop  int
-	}
-	tests := []data{
-		{"x{12,b}xxxxx", 1, 6},
-		{"x[124-5]xxxxx", 1, 7},
-		// complex, not expand at now
-		{"{x{12,{}}xxxxx", -1, -1},
-		{"x{12,{}}{{,13}", -1, -1},
-		{"{x{12,{}}{{,13}", -1, -1},
-		{"{{x{12,{}}{{,13}", -1, -1},
-		// unclosed
-		{"}some{", -1, -1},
-		{"}some", -1, -1},
-		{"some{", -1, -1},
-		{"]some[", -1, -1},
-		{"]some", -1, -1},
-		{"some[", -1, -1},
-	}
-
-	for _, tt := range tests {
-		start, stop := getPair(tt.in)
-		assert.Equal(t, tt.start, start, "start of %q", tt.in)
-		assert.Equal(t, tt.stop, stop, "stop of %q", tt.in)
 	}
 }
